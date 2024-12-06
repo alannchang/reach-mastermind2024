fde3330fde40a5856cb7a08d502a33a2262bf2a4
Alan Chang <alannnchang@gmail.com>
2024-12-05 21:57:30 -0800

    fixed logic for checking location

--- app/game.py
+++ app/game.py
@@ -10,27 +10,50 @@ def temp_random_list(nums):
 class GameSession:
 
     def __init__(self, total_random_nums=4, max_attempts=10):
-        self.random_list = temp_random_list(total_random_nums)
+        self.secret_code = temp_random_list(total_random_nums)
         self.max_attempts = max_attempts
         self.victory = False
+
+
+    def validate_input(self, player_code):
+        if len(player_code) != len(self.secret_code):
+            print("Invalid input! Please try again.")
+            return False
         
-    def check_correct(self, player_guess):
-        if len(self.random_list) != len(player_guess):
+    def check_correct(self, player_code):
+        if len(self.secret_code) != len(player_code):
             print("Invalid guess! Try again!")
             return
 
         correct_num = 0
         correct_loc = 0
+        incorrect_secret = []
+        incorrect_player = []
 
-        for i in range(0, len(player_guess)):
-            if self.random_list[i] == player_guess[i]:
-                correct_num += 1
+        for i in range(0, len(player_code)):
+            if self.secret_code[i] == player_code[i]:
                 correct_loc += 1
-            elif int(player_guess[i]) in self.random_list[i:]:
                 correct_num += 1
+            else:
+                incorrect_secret.append(self.secret_code[i])
+                incorrect_player.append(player_code[i])
+        
+        secret_count = {}
 
-        print(f"{correct_num} correct number(s) and {correct_loc} correct location(s).")
-        if correct_loc == len(self.random_list):
+        for num in incorrect_secret:
+            secret_count[num] = secret_count.get(num, 0) + 1
+
+        for num in incorrect_player:
+            if num in secret_count and secret_count[num] > 0:
+                correct_num += 1
+                secret_count[num] -= 1
+
+        if correct_num == 0 and correct_loc == 0:
+            print("All incorrect.")
+        else:
+            print(f"{correct_num} correct number(s) and {correct_loc} correct location(s).")
+
+        if correct_loc == len(self.secret_code) and correct_num == len(self.secret_code):
               self.victory = True
               return
 
@@ -38,15 +61,18 @@ class GameSession:
         print(f"{self.max_attempts} guesses remaining.")
 
     def game_loop(self):
-        print(self.random_list)
+        print(self.secret_code)
         while self.victory == False and self.max_attempts > 0:
-            player_input = input("What's your guess?")
-            # validate input here
-            guess_list = list(map(int, player_input.split()))
-            self.check_correct(guess_list)
+            player_input = input("What's your guess? Please follow this format: 1 2 3 4\n")
+            if self.validate_input(player_input) == False:
+                continue
+            player_code = list(map(int, player_input.split()))
+            self.check_correct(player_code)
+
         if self.victory:
             print("YOU WIN!")
         else:
             print("YOU LOSE!")
 
- 
+new_game = GameSession(4, 10)
+new_game.game_loop()
